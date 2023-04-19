91ebbff530f99beceba4a23f0dd81b8b4edf8085
nika <78690921+Niklaus2000@users.noreply.github.com>
2023-04-19 20:05:16 +0400

    -  create design

---
+++ ChatApp/ChatApp/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by MacBoobPro on 19.04.23.
+//
+
+import Foundation
---
+++ ChatApp/ChatApp/Presentation/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by MacBoobPro on 18.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
